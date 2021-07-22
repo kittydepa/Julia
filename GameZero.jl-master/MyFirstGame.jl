209bf9583c85fc1d7a6ca3232cdd07c3530fe424
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-22 17:05:13 +0200

    Add and image actor

--- GameZero.jl-master/MyFirstGame.jl
+++ GameZero.jl-master/MyFirstGame.jl
@@ -1,7 +1,12 @@
 # First, set the size of the game window
-HEIGHT = 400
-WIDTH = 400
+HEIGHT = 600
+WIDTH = 600
+BACKGROUND = colorant"#f5d1ff"
+heart = Actor("heart.png")
 
 function draw()
+    draw(heart)
+end
 
+function update()
 end
