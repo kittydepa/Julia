c8b95c53f70df9b407eae185c27946669b34d11a
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-07-22 18:00:08 +0200

    end

--- GameZero.jl-master/MyFirstGame.jl
+++ GameZero.jl-master/MyFirstGame.jl
@@ -7,16 +7,16 @@ heart_actor.pos = (280, 300)
 text_actor = TextActor("Kawaii desu", "8514oem")
 text_actor.pos = (350, 320)
 
-l = Line(50, 100, 350, 100) # The X and Y coordinates for the start and end point
-r = Rect(50, 100, 20, 50)
-c = Circle(300, 80, 20) # The radios, and X Y coordinates for the center
+# l = Line(50, 100, 350, 100) # The X and Y coordinates for the start and end point
+# r = Rect(50, 100, 20, 50)
+# c = Circle(300, 80, 20) # The radios, and X Y coordinates for the center
 
 function draw()
     draw(heart_actor)
     draw(text_actor)
-    draw(l, colorant"pink")
-    draw(c, colorant"white")
-    draw(r, colorant"purple")
+    # draw(l, colorant"pink")
+    # draw(c, colorant"white", fill = true)
+    # draw(r, colorant"purple", fill = true)
 end
 
 function update()
